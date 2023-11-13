d0f1fd43974dd754cfc7c58c9771d0ce9bbb0bc8
cameron001 <cward005@ucr.edu>
2023-11-13 13:33:34 -0800

    Added simple page for downloading log file

--- frontend/hello/urls.py
+++ frontend/hello/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import HomePageView
+from .views import HomePageView, LogPageView
 
 urlpatterns = [
+    path("settings/log/", LogPageView.as_view(), name="logpage"),
     path("",  HomePageView.as_view(), name="homepage"),
 ]
--- frontend/hello/views.py
+++ frontend/hello/views.py
@@ -3,3 +3,6 @@ from django.views.generic import TemplateView
 
 class HomePageView(TemplateView):
     template_name = "homepage.html"
+
+class LogPageView(TemplateView):
+    template_name = "logdownloadpage.html"

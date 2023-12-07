8080840f8b8a42ec67b8f483a09534727e5ed5ab
Hao F <haof@Haos-MacBook-Pro.local>
2023-12-07 14:56:09 -0800

    read 2d array from views, ui for empty and n/a cells

--- frontend/hello/views.py
+++ frontend/hello/views.py
@@ -1,4 +1,5 @@
 from django.views.generic import TemplateView
+from django.shortcuts import render
 
 
 class HomePageView(TemplateView):
@@ -12,4 +13,16 @@ class SettingsPageView(TemplateView):
 
 class GridPageView(TemplateView):
     template_name = "gridpage.html"
+
+    def get(self,request):
+        # placeholder
+        manifest = [[0]*12 for i in range(8)]
+        manifest[0][0],manifest[0][1],manifest[0][10],manifest[0][11] = -1,-1,-1,-1
+        manifest[1][1],manifest[2][1],manifest[0][2],manifest[1][11] = 1,1,1,1
+
+        context={
+            'manifest':manifest
+        }
+
+        return render(request,'gridpage.html',context)
 

fe65cc6a7adb76975d046cc5bcd7bfa2ced45025
Hao F <haof@Haos-MacBook-Pro.local>
2023-12-10 00:12:29 -0800

    ship model

--- frontend/hello/models.py
+++ frontend/hello/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+
+class ShipGrid(models.Model):
+    def __init__(self):
+        self.bay = None
+    
+    def read_bay(self,ship):
+        self.bay = [[val for val in row] for row in ship.bay]

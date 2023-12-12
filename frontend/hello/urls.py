00f43f3c54ffac8e4db4fd6b59a472734519dc47
Mohamedalr <mabue004@ucr.edu>
2023-12-12 14:52:03 -0800

    adding frontend stuff

--- frontend/hello/urls.py
+++ frontend/hello/urls.py
@@ -1,9 +1,15 @@
 from django.urls import path
-from .views import HomePageView, LogPageView, ManagePageView, TutorialPageView
+from .views import HomePageView, LogPageView, ManagePageView, TutorialPageView, MovesView, MovesLandUView, download_txt, clear_txt, home
 
 urlpatterns = [
     path("manage/", ManagePageView.as_view(), name="managepage"),
     path("manage/log/", LogPageView.as_view(), name="logpage"),
     path("",  HomePageView.as_view(), name="homepage"),
     path("tutorial/", TutorialPageView.as_view(), name="tutorialpage"),
+    path('download-txt/', download_txt, name='download_txt'),
+    path('clear-txt/', clear_txt, name='clear_txt'),
+    path('home/', home, name='home'),
+    path('moves/', MovesView.as_view(), name='movespage'),
+    path('movesLandU/', MovesLandUView.as_view(), name='movespageU'),
+    
 ]
--- frontend/hello/views.py
+++ frontend/hello/views.py
@@ -2,6 +2,13 @@ from django.views.generic import TemplateView
 from .forms import UploadFileForm
 from django.views import View
 from django.shortcuts import render
+from datetime import datetime
+from django.http import HttpResponse
+import os
+from backend.manifest import Manifest
+from backend import balancingAlg
+from backend.ship import Ship
+from backend import luAlg
 
 
 class HomePageView(View):
@@ -23,6 +30,17 @@ class HomePageView(View):
             # Render the template with a success message and uploaded file name
             return render(request, self.template_name, {'form': form, 'upload_success': True, 'uploaded_file_name': uploaded_file.name})
         return render(request, self.template_name, {'form': form})
+    
+
+    def post(self, request, *args, **kwargs):
+        sign_in_name = request.POST.get('SignIn', '')
+        if sign_in_name:
+            file_path = '/Users/mohamed/Desktop/PlanMaster/frontend/LogInFile/LogIn.txt'
+            timestamp = datetime.now().strftime("%m-%d-%Y %H:%M:%S")
+            with open(file_path, 'a') as file:
+                file.write(f'{sign_in_name} - {timestamp}\n')
+
+        return render(request, self.template_name, {'sign_in_name': sign_in_name})
 
 class LogPageView(TemplateView):
     template_name = "logdownloadpage.html"
@@ -30,6 +48,90 @@ class LogPageView(TemplateView):
 class ManagePageView(TemplateView):
     template_name = "managepage.html"
 
+def download_txt(request):
+    file_path = "/Users/mohamed/Desktop/PlanMaster/frontend/LogInFile/LogIn.txt"
+    # Check if the file exists
+    if os.path.exists(file_path):
+        # Open the file and read its content
+        with open(file_path, 'r') as file:
+            content = file.read()
+
+        response = HttpResponse(content, content_type='text/plain; charset=utf-8')
+        response['Content-Disposition'] = f'attachment; filename={os.path.basename(file_path)}'
+        return response
+    else:
+        return HttpResponse("File not found")
+    
+def clear_txt(request):
+    file_path = "/Users/mohamed/Desktop/PlanMaster/frontend/LogInFile/LogIn.txt"
+
+    if os.path.exists(file_path):
+        with open(file_path, 'w') as file:
+            file.write("")
+
+        return HttpResponse("File cleared successfully")
+
+def home(request):
+    return render(request, 'homepage.html') 
+
+class MovesView(View):
+    def get(self, request):
+        test_filename = "backend/ShipCase4.txt"
+        testManifest = Manifest()
+        new_ship = testManifest.read_manifest(test_filename)
+
+        res = balancingAlg.ucs(new_ship)
+
+        moves_data = []
+
+        while True:
+            move_info = {
+            "origin": res.lastMove[0],
+             "destination": res.lastMove[1],
+                }
+            moves_data.append(move_info)
+
+            if res.parent is None:
+                break
+
+            res = res.parent
+
+        moves_data.pop()
+        moves_data.reverse() 
+
+        context = {'moves': moves_data}
+        return render(request, 'moves.html', context)
+    
+class MovesLandUView(View):
+    def get(self, request):
+        test_filename = "backend/ShipCase1.txt"
+        testManifest = Manifest()
+        new_ship = testManifest.read_manifest(test_filename)
+        anylist = [(1,3)]
+
+        res = luAlg.ucs(new_ship, anylist)
+
+        moves_data = []
+
+        while True:
+            move_info = {
+            "origin": res.lastMove[0],
+             "destination": res.lastMove[1],
+                }
+            moves_data.append(move_info)
+
+            if res.parent is None:
+                break
+
+            res = res.parent
+            
+        moves_data.pop()
+        moves_data.reverse() 
+
+        context = {'moves': moves_data}
+        return render(request, 'movesLandU.html', context)
+
 class TutorialPageView(TemplateView):
     template_name = "tutorialpage.html"
+
 
